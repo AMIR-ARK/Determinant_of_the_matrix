34979d896c25ed45b2e4e7b8808bfc02f2c39435
AMIR-ARK <126375003+AMIR-ARK@users.noreply.github.com>
2023-06-25 08:13:52 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int determinant = CalculateDeterminant(matrix);
+
+        Console.WriteLine("The determinant of the matrix is: {0}", determinant);
+
+        Console.ReadKey();
+    }
+
+    static int CalculateDeterminant(int[,] matrix)
+    {
+        int det = 0;
+
+        det += matrix[0, 0] * matrix[1, 1] * matrix[2, 2];
+        det += matrix[0, 1] * matrix[1, 2] * matrix[2, 0];
+        det += matrix[0, 2] * matrix[1, 0] * matrix[2, 1];
+
+        det -= matrix[0, 2] * matrix[1, 1] * matrix[2, 0];
+        det -= matrix[0, 1] * matrix[1, 0] * matrix[2, 2];
+        det -= matrix[0, 0] * matrix[1, 2] * matrix[2, 1];
+
+        return det;
+    }
+}
